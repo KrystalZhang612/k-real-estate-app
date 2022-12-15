08e56f0e25a56d3d6d4e8af5f34249c240ef37fb
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-15 05:50:21 -0800

    last commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -4,9 +4,9 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': '9f92388fbamsh2e008548e615b32p1c410ajsne12c35239c3e',
+            'X-RapidAPI-Key': 'YOUR OWN BAYUT API',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     }); 
     return data; 
-}
+}; 

dfbd7a0113d66ce71a82e105fca8a289214c007c
Krystal Zhang <72481348+KrystalZhang612@users.noreply.github.com>
2022-12-17 23:51:23 -0800

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -4,7 +4,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': 'YOUR OWN BAYUT API',
+            'X-RapidAPI-Key': '9f92388fbamsh2e008548e615b32p1c410ajsne12c35239c3e',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     }); 
